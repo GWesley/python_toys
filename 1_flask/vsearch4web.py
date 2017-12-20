927cd5f981cfb6446b3c6b01e599c2ecf114a5e0
zhangzhiyong <zhangzhiyong-pd@360.cn>
2017-12-20 14:07:05 +0800

    add text log

--- 1_flask/vsearch4web.py
+++ 1_flask/vsearch4web.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect 
+from flask import Flask, render_template, request, escape
 app = Flask(__name__)
+
 
 @app.route('/')
 @app.route('/entry')
@@ -8,15 +9,30 @@ def entry_page() -> 'html':
                             the_title='Welcome to search4letters on the web!',
                             )
 
+
 @app.route('/search4', methods=["POST"])
 def do_search() -> str:
     phrase = request.form['phrase'] 
     letters = request.form['letters']
+    result = 'what ever'
+    log_request(request, result)
     return render_template('results.html',
                             the_title='Here are your result',
                             the_phrase=phrase,
                             the_letters=letters,
-                            the_result='what ever'
+                            the_results='what ever'
                             )
+
+@app.route('/viewlog')
+def view_the_log() -> str:
+    with open('vsearch.log') as log :
+        contents = log.read()
+        return escape(contents)
+
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log','a') as log:
+        print(req, res, file=log)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
